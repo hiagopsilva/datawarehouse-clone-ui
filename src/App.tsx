d1f778cd176445dd7dbe8650dc8834ef3da5be91
hiagopsilva <hiagohps360@gmail.com>
2020-08-01 00:50:19 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
+
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 
 import GlobalStyles from './styles/global';
 import Routes from './routes/index';
 
-const App: React.FC = () => (
-  <>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-    <GlobalStyles />
-  </>
-);
+const App: React.FC = () => {
+  const [theme, setTheme] = useState(light);
+
+  // const toogleTheme = () => {
+  //   setTheme(theme.title === 'light' ? dark : light);
+  // };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+        <GlobalStyles />
+      </>
+    </ThemeProvider>
+  );
+};
 
 export default App;
