5504dc03eaa7768b2aac4543d7ca8cc1e78bc3ba
hiagopsilva <hiagohps360@gmail.com>
2020-08-01 00:48:32 -0300

    add dark theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#F6FAFD',
+    secundary: '#9C69E2',
+    tertiary: '#F063B8',
+
+    background: '#212353',
+
+    fontPrimaryColor: '#FFF',
+    fontSecundaryColor: '#4B5D68',
+
+    buttonShadow: 'rgba(75, 93, 104, 0.1)',
+  },
+};
